f4ebe0ba529b09b8ed7460e61795fcb58fca8e17
Jesus Alvarado Garzon <xtornasol512@gmail.com>
2018-06-05 18:56:19 -0500

    Revert "add function create_transaction and mofiy view"
    This reverts commit ec9221d508c477b8162295bd2143a29ccf87f6c3.

--- prescryptchain/api/views.py
+++ prescryptchain/api/views.py
@@ -12,7 +12,7 @@ from rest_framework import mixins, generics
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 # our models
-from blockchain.models import Block, Prescription, Medication, Transaction
+from blockchain.models import Block, Prescription, Medication
 
 # Define router
 router = routers.DefaultRouter()
@@ -39,18 +39,6 @@ class PrescriptionSerializer(serializers.ModelSerializer):
 
     class Meta:
         #model = Prescription
-        model = Transaction
-        fields = (
-            'id',
-            'timestamp',
-            'signature',
-            'previous_hash',
-            'raw_size',
-            'rxid',
-            'is_valid',
-            'block',
-        )
-    class Meta:
         model = Prescription
         fields = (
             'id',
@@ -76,10 +64,11 @@ class PrescriptionSerializer(serializers.ModelSerializer):
         #read_only_fields = ('id', 'rxid', 'previous_hash', 'is_valid', 'block')
 
     def create(self, validated_data):
-        rx = Prescription.objects.create_rx(data=validated_data)
-        tx = Transaction.objects.create_tx(data=validated_data)
+        #rx = Prescription.objects.create_rx(data=validated_data)
+        tx = Prescription.objects.create_tx(data=validated_data)
         #return rx Transaction
-        return rx
+        return tx
+
 
 class PrescriptionViewSet(viewsets.ModelViewSet):
     """ Prescription Viewset """
